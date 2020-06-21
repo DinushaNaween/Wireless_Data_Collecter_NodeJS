57f88d7cba8e0a22083fae133182bfc821e1f991
Dinusha <dinusha.16@itfac.mrt.ac.lk>
2020-06-21 20:17:22 +0530

    changes

--- tests/testing.js
+++ tests/testing.js
@@ -1,25 +1,10 @@
-// A simple promise that resolves after a given time
-const timeOut = (t) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (t === 2000) {
-        reject(`Rejected in ${t}`)
-      } else {
-        resolve(`Completed in ${t}`)
-      }
-    }, t)
-  })
+let test = '-20';
+let test2 = '30';
+let test3 = '19';
+
+console.log(parseInt(test));
+console.log(parseInt(test2));
+
+if (parseInt(test3) < parseInt(test2)) {
+  console.log('test 3 is lower than test2')
 }
-
-const durations = [1000, 2000, 3000]
-
-const promises = []
-
-durations.map((duration) => {
-  promises.push(timeOut(duration)) 
-})
-
-// We are passing an array of pending promises to Promise.all
-Promise.all(promises)
-.then(response => console.log(response)) // Promise.all cannot be resolved, as one of the promises passed got rejected.
-.catch(error => console.log(`Error in executing ${error}`)) // Promise.all throws an error.
