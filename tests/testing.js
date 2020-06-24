c0056bfb5cc918dae7816d47affae226f456f6b1
Dinusha <dinusha.16@itfac.mrt.ac.lk>
2020-06-24 23:11:51 +0530

    change

--- tests/testing.js
+++
@@ -1,5 +0,0 @@
-var array1 = ["cat", "sum","fun", "run", "hut"];
-var array2 = ["bat", "cat","dog","sun", "hut", "gut"];
-
-const missedNodes = [...new Set(array1.filter(element => !array2.includes(element)))];
-console.log(missedNodes);
