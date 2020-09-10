b6e68d47383b4d7558a913b69660141791c73f79
Dinusha <dinusha.16@itfac.mrt.ac.lk>
2020-09-10 07:52:38 +0530

    db connection check

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,17 @@
 const { createConnection } = require('mysql');
 const logger = require('../middlewares/logger.middleware');
 
-const HOST = process.env.HOST;
-const USER = process.env.USER;
-const PASSWORD = process.env.PASSWORD;
-const PORT = process.env.PORT;
-const DB = process.env.DB;
+// const HOST = process.env.HOST;
+// const USER = process.env.USER;
+// const PASSWORD = process.env.PASSWORD;
+// const PORT = process.env.PORT;
+// const DB = process.env.DB;
+
+const HOST = 'freedb.tech';
+const USER = 'freedb_teamgreen'
+const PASSWORD = '123456789'
+const PORT = '3306'
+const DB = 'freedb_wdc'
 
 const connection = createConnection({
   host: HOST,
